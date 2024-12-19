2c0c71019280fbc9e6597b563851e325ecfe32c9
joshbae119 <mclceo119@gmail.com>
2024-12-19 12:33:06 +0900

    2024.12.19 회원가입중

--- Board/src/main/java/com/springboot/biz/user/UserRepository.java
+++ Board/src/main/java/com/springboot/biz/user/UserRepository.java
@@ -1,7 +1,13 @@
 package com.springboot.biz.user;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
 
 public interface UserRepository extends JpaRepository<SiteUser, Long>{
+	
+	Optional<SiteUser> findByUsername(String username);
 
 }
---
+++ Board/src/main/java/com/springboot/biz/user/UserRole.java
@@ -0,0 +1,16 @@
+package com.springboot.biz.user;
+
+import lombok.Getter;
+
+@Getter
+public enum UserRole {
+	
+	ADMIN("ROLE_ADMIN"), USER("ROLE_USER");
+	
+	private UserRole(String value) {
+		this.value = value;
+	}
+	
+	private String value;
+
+}
